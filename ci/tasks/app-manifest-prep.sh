63881d9b1af508589ccb6a39f90d3a26bbcca38b
Daxter Mateo <DaxterM@users.noreply.github.com>
2018-12-04 16:16:08 -0500

    Update app-manifest-prep.sh

--- ci/tasks/app-manifest-prep.sh
+++ ci/tasks/app-manifest-prep.sh
@@ -4,15 +4,4 @@ set -xe
 Version=`cat version/number`
 
 
-#echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 1G \n  random-route: true \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
-
-cat > app-manifest-output/manifest.yml << EOF
----
-applications:
-- name: Spring-Music-CIDemo
-  memory: 1G
-  random-route: true
-  routes:
-  - route: dax-springmusic.cfapps.io
-  path: /tmp/build/put/music-release/
-EOF
+echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 1G \n  host: Dax-Spring-Music-CICD-Demo \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
